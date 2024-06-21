6733edcaae0170755838e607bec8b742eaee0bc9
Loïc Huvé <loic.huve@onefirstgroup.com>
2024-06-21 16:46:16 +0200

    base working example with barcode scanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import { FC } from "react";
+import { Barcode, BarcodeScanner } from "@capacitor-mlkit/barcode-scanning";
+import { FC, useState } from "react";
+
+import './style.css';
 
 const App: FC = () => {
+    const [barcode, setBarcode] = useState<string>();
+    const onClickBarcodeScan = async () => {
+        try {
+            const res = await new Promise<Barcode>(async (resolve) => {
+                document.querySelector("body")?.classList.add("barcode-scanner-active");
+
+                const listener = await BarcodeScanner.addListener("barcodeScanned", async (result) => {
+                    await listener.remove();
+                    document.querySelector("body")?.classList.remove("barcode-scanner-active");
+                    await BarcodeScanner.stopScan();
+                    resolve(result.barcode);
+                });
+
+                await BarcodeScanner.startScan();
+            });
+
+            setBarcode(res.rawValue);
+        } catch (error) {
+            console.error(error);
+        }
+    };
     return (
-        <h1>Hello</h1>
+        <>
+            <h1>Hello</h1>
+            <button onClick={onClickBarcodeScan}>SCAN BARCODE</button>
+            <p>scanned barcode : {barcode}</p>
+        </>
     );
 };
 
